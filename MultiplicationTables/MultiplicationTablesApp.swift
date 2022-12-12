7f0eb01dc553cafcffbe88f6825633a8aba776b1
Philipp Sanktjohanser <venture@mein.gmx>
2022-12-12 08:33:35 +0100

    Initial Commit

---
+++ MultiplicationTables/MultiplicationTablesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiplicationTablesApp.swift
+//  MultiplicationTables
+//
+//  Created by Philipp Sanktjohanser on 12.12.22.
+//
+
+import SwiftUI
+
+@main
+struct MultiplicationTablesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
